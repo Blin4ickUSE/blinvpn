4ff31495aa0d0bf4834e6ad4acaa797f097de308
IIapick <158780807+Blin4ickUSE@users.noreply.github.com>
2026-01-09 19:02:45 +0300

    Add files via upload

---
+++ panel/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3001
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3001
+  }
+})
+
