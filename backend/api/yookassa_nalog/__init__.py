60b1b16c00287de9366d5dc01bd4f32b597c8cd2
IIapick <158780807+Blin4ickUSE@users.noreply.github.com>
2026-01-23 18:46:47 +0300

    Delete backend/api/yookassa_nalog directory

--- backend/api/yookassa_nalog/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Модуль для автоматической оплаты налога (самозанятость) через API "Мой Налог"
-Интегрируется с YooKassa для создания чеков после успешных платежей
-"""
-
-from .client import NalogClient
-from .service import NalogService, nalog_service
-
-__all__ = ['NalogClient', 'NalogService', 'nalog_service']
